203e0918cf60b23dd908f337c7289123e3be0092
Leon Hannah Tabak <l.tabak@ieee.org>
2021-11-29 10:40:41 -0600

    Demonstrate use of Git.

---
+++ src/main/java/com/eonsahead/pelican/Pelican.java
@@ -0,0 +1,12 @@
+
+package com.eonsahead.pelican;
+
+
+public class Pelican {
+
+    public static void main(String[] args) {
+        System.out.append( "I live in Florida but visit Iowa." );
+    } // main( String [] )
+    
+} // Pelican
+
